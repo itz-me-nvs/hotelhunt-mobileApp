bd4adbf1b6d650729c7093dde7b0428dbbc134ce
MUHAMMED NAVAS <muhammednavas320203@gmail.com>
2023-05-10 21:55:24 +0530

    Animated Page header

--- lib/components/UI/PageHeader.tsx
+++ lib/components/UI/PageHeader.tsx
@@ -1,7 +1,12 @@
 import {useNavigation} from '@react-navigation/native';
-import {Image, StyleSheet, View} from 'react-native';
+import {memo} from 'react';
+import {Animated, Image, StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {useTheme} from 'styled-components';
+import {
+  HEADER_MAX_HEIGHT,
+  HEADER_MIN_HEIGHT,
+} from '../../shared/constants/app.constant';
 import {
   PageHeaderComponentType,
   ThemeType,
@@ -12,26 +17,50 @@ const PageHeader = ({
   title,
   subtitle,
   fancy = false,
+  routerPath = 'HomePage',
   icon,
+  animatedValue,
+  ScrollBackgroundColor,
   children,
   ...rest
 }: PageHeaderComponentType) => {
+  // image assests list
   const imageList = {
     welcome: require(`../../assets/icon/welcome.png`),
     notifications: require('../../assets/icon/notifications.png'),
     addhotel: require('../../assets/icon/addhotel.png'),
   };
 
-  // dynamic import
+  // Animated Vector Icon
+  const AnimatedIcon = Animated.createAnimatedComponent(Icon);
 
-  const imageSource = imageList[icon as keyof typeof imageList];
+  // header height & background animation
   const navigation = useNavigation();
   const theme = useTheme() as ThemeType;
-  const styles = getStyles(theme);
+
+  const height = animatedValue?.interpolate({
+    inputRange: [0, HEADER_MAX_HEIGHT],
+    outputRange: [HEADER_MAX_HEIGHT, HEADER_MIN_HEIGHT],
+    extrapolate: 'clamp',
+  });
+
+  const backgroundColor = animatedValue?.interpolate({
+    inputRange: [0, HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT],
+    outputRange: [theme['pageHeader-50'], theme.white],
+    extrapolate: 'clamp',
+  });
+  const styles = getStyles(theme, backgroundColor);
+
   return (
     <>
       {fancy ? (
-        <View style={styles.fancyPageHeaderContainer}>
+        <View
+          style={[
+            styles.fancyPageHeaderContainer,
+            {
+              height: 100,
+            },
+          ]}>
           <View style={styles.fancyItemContainer}>
             <View
               style={{
@@ -39,11 +68,11 @@ const PageHeader = ({
                 flexDirection: 'row',
                 alignItems: 'center',
               }}>
-              <Icon
+              <AnimatedIcon
                 name="location-outline"
                 size={25}
                 color={theme.secondary}
-                onPress={() => navigation.navigate('OTPVerification' as never)}
+                onPress={() => navigation.navigate(routerPath as never)}
               />
               <Typography
                 color="secondary"
@@ -62,8 +91,6 @@ const PageHeader = ({
                 marginTop: 5,
                 marginLeft: 5,
                 width: '90%',
-
-                // text ellipsis
               }}>
               {subtitle}
             </Typography>
@@ -78,44 +105,57 @@ const PageHeader = ({
           <View style={styles.curve}></View>
         </View>
       ) : (
-        <View style={styles.pageHeaderContainer}>
-          <View style={styles.itemContainer}>
-            <Icon
+        <Animated.View
+          style={[
+            styles.pageHeaderContainer,
+            {
+              height: height,
+            },
+          ]}>
+          <Animated.View style={styles.itemContainer}>
+            <AnimatedIcon
               name="arrow-back"
               size={25}
               color={theme.secondary}
-              onPress={() => navigation.navigate('OTPVerification' as never)}
+              onPress={() => navigation.navigate(routerPath as never)}
             />
-            <Typography color="secondary" fontFamily="Bold" fontSize={14}>
+            <Typography
+              color={theme.secondary as keyof ThemeType}
+              fontFamily="Bold"
+              fontSize={14}>
               {title}
             </Typography>
 
-            <Typography color="secondary-75" fontFamily="Regular" fontSize={10}>
+            <Typography
+              color={theme['secondary-75'] as keyof ThemeType}
+              fontFamily="Regular"
+              fontSize={10}>
               {subtitle}
             </Typography>
-          </View>
+          </Animated.View>
 
-          {/* {iconName != '' && ( */}
           <Image source={imageList[icon as never]} style={styles.headerIcon} />
-          {/* )} */}
-        </View>
+        </Animated.View>
       )}
     </>
   );
 };
 
-const getStyles = (theme: ThemeType) =>
+const getStyles = (theme: ThemeType, ScrollBackgroundColor: string) =>
   StyleSheet.create({
     pageHeaderContainer: {
-      backgroundColor: theme.pageHeader,
-      height: 100,
+      backgroundColor: ScrollBackgroundColor,
       width: '100%',
       justifyContent: 'space-between',
       alignItems: 'center',
       display: 'flex',
       flexDirection: 'row',
       padding: 15,
-      position: 'relative',
+
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
     },
     itemContainer: {
       display: 'flex',
@@ -139,13 +179,12 @@ const getStyles = (theme: ThemeType) =>
       alignItems: 'flex-start',
       display: 'flex',
       flexDirection: 'row',
-      position: 'relative',
     },
 
     curve: {
       width: '100%',
       height: 100,
-      backgroundColor: theme.pageHeader,
+      backgroundColor: theme['pageHeader-50'],
       position: 'absolute',
       top: 0,
       left: 0,
@@ -171,4 +210,4 @@ const getStyles = (theme: ThemeType) =>
     },
   });
 
-export default PageHeader;
+export default memo(PageHeader);
